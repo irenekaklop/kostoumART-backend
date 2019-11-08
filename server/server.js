91169d66108d99489268c858a2707235707ed69d
Eirini Kaklopoulou <eirini.kaklopoulou@gmail.com>
2019-11-08 17:22:47 +0200

    Functions for get and post added

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.listen(8108, function () {
 var dbConn = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'culture123!',
+    password: '',
     database: 'theaterdb'
 });
   
@@ -36,19 +36,79 @@ app.get('/', function (req, res) {
     res.send('Hello from my api.');
 });
 
-// fetch all costumes
-app.post('/costumes', function (req, res) {
-    dbConn.query("SELECT costumes.costume_name, costumes.description, costumes.sex, uses.name as use_name, costumes.material, costumes.technique, costumes.location, costumes.location_influence, costumes.designer, theatrical_plays.title as tp_title, costumes.parts, costumes.actors FROM costumes INNER JOIN uses ON costumes.useID = uses.useID INNER JOIN theatrical_plays ON costumes.theatrical_play_id=theatrical_plays.theatrical_play_id", function (error, results, fields){
-        if(error) throw error;
-        res.send(JSON.stringify(results))
+/*COSTUMES*/
+
+//show all costumes
+app.get('/costumes',(req, res) => {
+    let sql = "SELECT costumes.costume_name, costumes.description, costumes.sex, uses.name as use_name, costumes.material, costumes.technique, costumes.location, costumes.location_influence, costumes.designer, theatrical_plays.title as tp_title, costumes.parts, costumes.actors FROM costumes INNER JOIN uses ON costumes.useID = uses.useID LEFT JOIN theatrical_plays ON costumes.theatrical_play_id=theatrical_plays.theatrical_play_id";
+    let query =  dbConn.query(sql, (err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+  });
+   
+//show single costume
+app.get('/costumes/:id',(req, res) => {
+    let sql = "SELECT * FROM costumes WHERE costume_id="+req.params.id;
+    let query = dbConn.query(sql, (err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+  });
+
+//add new costume
+app.post('/costumes',(req, res) => {
+    let data ={costume_name: req.body.name, description: req.body.descr, use_name: req.body.u_value, technique: req.body.t_value, sex: req.body.s_value, material: req.body.m_value,
+        actors: req.body.actors, location: req.body.location, location_influence: req.body.location_influence,
+        designer: req.body.designer, theatrical_play: req.body.tp_value, parts: req.body.parts  };
+    console.log(data);
+    let sql = "INSERT INTO costumes SET costume_name= '"+data.costume_name+"', description= '"+data.description+"', technique= '"+data.technique+"', sex= '"+data.sex+"', material= '"+data.material+"', actors= '"+data.actors+"', location= '"+data.location+"', location_influence= '"+data.location_influence+"', designer= '"+data.designer+"', parts= '"+data.parts+"', useID= ( SELECT useID FROM uses WHERE name = '"+data.use_name+"'), theatrical_play_id = ( SELECT theatrical_play_id FROM theatrical_plays WHERE title = '"+data.theatrical_play+"')";
+    let query = dbConn.query(sql, data, (err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+  });
+
+/*USES*/
+//show all uses
+app.get('/uses',(req, res) => {
+    let sql = "SELECT * FROM uses";
+    let query =  dbConn.query(sql, (err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+  });
+
+//add new use
+app.post('/uses', (req, res) => {
+    let data ={name: req.body.name, use_category: req.body.category, description: req.body.description, customs: req.body.customs};
+    let sql = "INSERT INTO uses SET ?";
+    let query = dbConn.query(sql, data,(err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
     });
 });
 
-app.get('/costumes', function (req, res) {
-    dbConn.query("SELECT costumes.costume_name, costumes.description, costumes.sex, uses.name as use_name, costumes.material, costumes.technique, costumes.location, costumes.location_influence, costumes.designer, theatrical_plays.title as tp_title, costumes.parts, costumes.actors FROM costumes INNER JOIN uses ON costumes.useID = uses.useID INNER JOIN theatrical_plays ON costumes.theatrical_play_id=theatrical_plays.theatrical_play_id", function (error, results, fields){
-        if(error) throw error;
-        res.send(JSON.stringify(results))
+/*THEATRICAL PLAYS*/
+
+//show all theatrical plays
+app.get('/tps',(req, res) => {
+    let sql = "SELECT * FROM theatrical_plays";
+    let query =  dbConn.query(sql, (err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
     });
-}); 
+});
+
+//add new theatrical play
+app.post('/tps', (req, res) => {
+    let data ={title: req.body.title, date: req.body.date, actors: req.body.actors, director: req.body.director, theater: req.body.theater};
+    let sql = "INSERT INTO theatrical_plays SET ?";
+    let query = dbConn.query(sql, data,(err, results) => {
+      if(err) throw err;
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+    });
+});
+
 
 module.exports = app;
