e3bd132e0c3e8cfea5d4a0ca79dbcf6911d075da
Eirini Kaklopoulou <eirini.kaklopoulou@gmail.com>
2019-12-19 16:14:47 +0200

    Erase Bcrypt

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -2,47 +2,11 @@ const express = require('express')
 const users = express.Router()
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
 
 const User = require('../models/User');
 users.use(cors())
 
 process.env.SECRET_KEY = 'secret'
-
-users.post('/register', (req, res) => {
-  const today = new Date()
-  const userData = {
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  }
-
-  User.findOne({
-    where: {
-      email: req.body.email
-    }
-  })
-    //TODO bcrypt
-    .then(user => {
-      if (!user) {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          userData.password = hash
-          User.create(userData)
-            .then(user => {
-              res.json({ status: user.email + 'Registered!' })
-            })
-            .catch(err => {
-              res.send('error: ' + err)
-            })
-        })
-      } else {
-        res.json({ error: 'User already exists' })
-      }
-    })
-    .catch(err => {
-      res.send('error: ' + err)
-    })
-})
 
 users.post('/login', (req, res) => {
   console.log(req.body)
