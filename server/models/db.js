07af361d4056ce3906c4fd5e9c0edacb7c266d4a
Irene Kaklopoulou <eirini.kaklopoulou@gmail.com>
2020-04-16 11:42:31 +0300

    Fixing automatic re-connection

--- server/models/db.js
+++ server/models/db.js
@@ -1,38 +1,87 @@
-const mysql = require("mysql");
+//- MYSQL Module
+try{
+  var mysql_npm = require("mysql");
+}catch(err){
+  console.log("Cannot find `mysql` module. Is it installed ? Try `npm install mysql` or `npm install`.");
+}
+
 const dbConfig = require("../config/db.config.js");
 
-// Define the connection to the database
-var connection;
+//  Create the connection variable
+var connection = mysql_npm.createConnection({
+  host: dbConfig.host,
+  user: dbConfig.user,
+  password: dbConfig.password,
+  database: dbConfig.database
+});
 
-function handleConnection() {
-  // Recreate the connection, since the old one cannot be reused.
-  connection = mysql.createConnection({
-    host: dbConfig.host,
-    user: dbConfig.user,
-    password: dbConfig.password,
-    database: dbConfig.database
-  });                                                   
+//  Establish a new connection
+connection.connect(function(err){
+  if(err) {
+      // mysqlErrorHandling(connection, err);
+      console.log("\n\t *** Cannot establish a connection with the database. ***");
 
-  connection.connect(function(err) {           // The server is either down
-    if(err) {                                 // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleConnection, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to                                    // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-    else{
-      console.log("Successfully connected to the database.");
-    }       
-  });                             
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleConnection();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
+      connection = reconnect(connection);
+  }else {
+      console.log("\n\t *** New connection established with the database. ***")
+  }
+});
+
+//  Reconnection function
+function reconnect(connection){
+  console.log("\n New connection tentative...");
+
+  // Destroy the current connection variable
+  if(connection) connection.destroy();
+
+  // Create a new one
+  var connection = mysql_npm.createConnection(db_config);
+
+  // Try to reconnect
+  connection.connect(function(err){
+      if(err) {
+          //- Try to connect every 2 seconds.
+          setTimeout(reconnect, 2000);
+      }else {
+          console.log("\n\t *** New connection established with the database. ***")
+          return connection;
+      }
   });
 }
 
-handleConnection();
+// Error listener
+connection.on('error', function(err) {
+
+  // The server close the connection.
+  if(err.code === "PROTOCOL_CONNECTION_LOST"){    
+      console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+      connection = reconnect(connection);
+  }
+
+  // Connection in closing
+  else if(err.code === "PROTOCOL_ENQUEUE_AFTER_QUIT"){
+      console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+      connection = reconnect(connection);
+  }
+
+  // Fatal error : connection variable must be recreated
+  else if(err.code === "PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR"){
+      console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+      connection = reconnect(connection);
+  }
+
+  // Error because a connection is already being established
+  else if(err.code === "PROTOCOL_ENQUEUE_HANDSHAKE_TWICE"){
+      console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+  }
+
+  // Anything else
+  else{
+      console.log("/!\\ Cannot establish a connection with the database. /!\\ ("+err.code+")");
+      connection = reconnect(connection);
+  }
+
+});
 
 module.exports = connection;
+
